34bec277fbfd2c5dd1797baee0259440566bfc38
Browni Wilson <browniwils@gmail.com>
2023-10-02 08:53:24 +0000

    update storage engine and authors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def count(self, cls=None):
+        """Count class instances."""
+        return (len(self.all(cls)))
